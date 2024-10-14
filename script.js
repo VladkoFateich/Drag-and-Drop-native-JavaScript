59e9365aa44eeea3366c390b6f1fd7a1f941f8ab
Vladko Fateich <Fateev_wbd@mail.ru>
2024-10-14 21:22:47 +0500

    add touch

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const products = document.querySelectorAll('.products');
 const cart = document.querySelector('#cart');
 const main = document.querySelector('#main');
 const button = document.querySelector('#button');
-let current;
-let counter = 0;
+let current,
+  counter = 0,
+  offsetX,
+  offsetY;
 
 products.forEach(function (product) {
   product.addEventListener('dragstart', function (e) {
@@ -23,6 +25,15 @@ cart.addEventListener('drop', function addProduct() {
     button.style.display = 'block';
     this.removeEventListener('drop', addProduct);
   }
+});
+
+elem1.addEventListener('touchstart', function (e) {
+  offsetX = e.offsetX;
+  offsetY = e.offsetY;
+});
+elem1.addEventListener('touchend', function (e) {
+  elem1.style.top = e.pageY - offsetY + 'px';
+  elem1.style.top = e.pageX - offsetX + 'px';
 });
 
 // main.addEventListener('dragover', function (e) {
